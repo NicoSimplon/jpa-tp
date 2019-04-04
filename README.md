219fa19bfc7ace136ea73d2f52dc4a7b2edb1eb6
Nicolas <marty.nicolas11@laposte.net>
2019-04-04 14:33:11 +0200

    Ajout readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# jpa-tp
+# Tavaux Pratiques sur JPA
+
+Création d'une application JAVA utilisant la techno JPA, tout en simulant un système de prêt dans une bibliothèque.
+
+
--- src/main/java/dev/jpa_tp/Client.java
+++ src/main/java/dev/jpa_tp/Client.java
@@ -1,5 +1,20 @@
 package dev.jpa_tp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Modèle de l'entitée Livre
+ * 
+ * @author Nicolas
+ *
+ */
+@Entity // obligatoire
+@Table(name="livre")
 public class Client {
 
 }
